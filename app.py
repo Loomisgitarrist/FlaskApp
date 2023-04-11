1d7b18ab1a0b2ba0aac609a4a7e36f950bf94e58
Loomis Green <53963559+Loomisgitarrist@users.noreply.github.com>
2023-04-11 10:52:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, request, send_file
+from flask import Flask, render_template, request, send_file
+import qrcode
 import os
-
 app = Flask(__name__)
-
-@app.route('/api/convert', methods=['POST'])
-def convert_song():
-    file = request.files['file']
-    filename = file.filename
-    file.save(filename)
-    os.system('your_conversion_command_here') # replace this with the command to convert the song using Falcon Cubase 12
-    converted_file = filename + '.cpr' # replace this with the name of the converted file
-    return send_file(converted_file, as_attachment=True)
-
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/generate', methods=['POST'])
+def generate():
+    url = request.form['url']
+    # Generate QR code
+    filename = f'{os.urandom(8).hex()}.png'
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="white", back_color="black")
+    img.save(os.path.join('static', 'qr_codes', filename))
+    # Send QR code as attachment
+    try:
+        return send_file(os.path.join('static', 'qr_codes', filename),
+                         as_attachment=True)
+    except Exception as e:
+        return str(e)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
